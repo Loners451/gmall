a04044f64adc8018f0ec116277f515037b6b694e
Loners <977036451@qq.com>
2019-10-18 20:31:22 +0800

    day8

--- gmall-manage-service/src/main/java/com/example/service/impl/SkuServiceImpl.java
+++ gmall-manage-service/src/main/java/com/example/service/impl/SkuServiceImpl.java
@@ -1,16 +1,20 @@
 package com.example.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.fastjson.JSON;
 import com.example.mapper.PmsSkuAttrValueMapper;
 import com.example.mapper.PmsSkuImageMapper;
 import com.example.mapper.PmsSkuInfoMapper;
 import com.example.mapper.PmsSkuSaleAttrValueMapper;
+import com.example.util.RedisUtil;
 import com.gmall.bean.PmsSkuAttrValue;
 import com.gmall.bean.PmsSkuImage;
 import com.gmall.bean.PmsSkuInfo;
 import com.gmall.bean.PmsSkuSaleAttrValue;
 import com.gmall.service.SkuService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.Jedis;
 
 import java.util.List;
 
@@ -32,6 +36,9 @@ public class SkuServiceImpl implements SkuService {
 
     @Autowired
     PmsSkuImageMapper pmsSkuImageMapper;
+
+    @Autowired
+    RedisUtil redisUtil;
 
     @Override
     public void saveSkuInfo(PmsSkuInfo pmsSkuInfo) {
@@ -61,18 +68,62 @@ public class SkuServiceImpl implements SkuService {
         }
     }
 
-    @Override
-    public PmsSkuInfo getSkuById(String skuId) {
-
+    public PmsSkuInfo getSkuByIdFromDb(String skuId) {
+        // sku商品对象
         PmsSkuInfo pmsSkuInfo = new PmsSkuInfo();
         pmsSkuInfo.setId(skuId);
+        PmsSkuInfo skuInfo = pmsSkuInfoMapper.selectOne(pmsSkuInfo);
 
+        // sku的图片集合
         PmsSkuImage pmsSkuImage = new PmsSkuImage();
         pmsSkuImage.setSkuId(skuId);
         List<PmsSkuImage> pmsSkuImages = pmsSkuImageMapper.select(pmsSkuImage);
+        skuInfo.setSkuImageList(pmsSkuImages);
+        return skuInfo;
+    }
 
-        pmsSkuInfo.setSkuImageList(pmsSkuImages);
-        return    pmsSkuInfoMapper.selectOne(pmsSkuInfo);
+    @Override
+    public PmsSkuInfo getSkuById(String skuId) {
+        PmsSkuInfo pmsSkuInfo = new PmsSkuInfo();
+        // 链接缓存
+        Jedis jedis = redisUtil.getJedis();
+        // 查询缓存
+        String skuKey = "sku:"+skuId+":info";
+        String skuJson = jedis.get(skuKey);
+
+        if(StringUtils.isNotBlank(skuJson)){//if(skuJson!=null&&!skuJson.equals(""))
+            pmsSkuInfo = JSON.parseObject(skuJson, PmsSkuInfo.class);
+        }else{
+            // 如果缓存中没有，查询mysql
+
+            // 设置分布式锁
+            String OK = jedis.set("sku:" + skuId + ":lock", "1", "nx", "px", 10);
+            if(StringUtils.isNotBlank(OK)&&OK.equals("OK")){
+                // 设置成功，有权在10秒的过期时间内访问数据库
+                pmsSkuInfo =  getSkuByIdFromDb(skuId);
+                if(pmsSkuInfo!=null){
+                    // mysql查询结果存入redis
+                    jedis.set("sku:"+skuId+":info",JSON.toJSONString(pmsSkuInfo));
+                }else{
+                    // 数据库中不存在该sku
+                    // 为了防止缓存穿透将，null或者空字符串值设置给redis
+                    jedis.setex("sku:"+skuId+":info",60*3,JSON.toJSONString(""));
+                }
+            }else{
+                // 设置失败，自旋（该线程在睡眠几秒后，重新尝试访问本方法）
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return getSkuById(skuId);
+            }
+        }
+
+
+        jedis.close();
+
+        return pmsSkuInfo;
     }
 
     @Override
---
+++ gmall-service-util/src/main/java/com/example/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package com.example.config;
+
+import com.example.util.RedisUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Moses
+ * @version 1.0
+ * @date 2019/10/18 9:00
+ */
+@Configuration
+public class RedisConfig {
+    //读取配置文件中的redis的ip地址
+    @Value("${spring.redis.host:disabled}")
+    private String host;
+    @Value("${spring.redis.port:0}")
+    private int port;
+    @Value("${spring.redis.database:0}")
+    private int database;
+    @Bean
+    public RedisUtil getRedisUtil(){
+        if(host.equals("disabled")){
+            return null;
+        }
+        RedisUtil redisUtil=new RedisUtil();
+        redisUtil.initPool(host,port,database);
+        return redisUtil;
+    }
+
+}   
